9c69a84ee4c86d09cdb7ec7a22957a399b376d27
Сергей <45902301+leninware@users.noreply.github.com>
2022-12-06 23:40:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import os
+import json
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 app = Flask(__name__)
+url = os.getenv("DATABASE_URL")
 
+connection = psycopg2.connect(url)
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute('Select * from cars;')
+            data = cursor.fetchall()
+    return render_template('index.html', data=data)
 
+@app.route("/api/car/<int:n>", methods=['GET'])
+def get_info_taxi(n):
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute('Select taxi_name, tarif_name, price from taxi, Сhoice, cars where cars.car_id = {0} AND taxi.taxi_id = Сhoice.tarif_id AND Cars.car_id = Сhoice.car_id;'.format(n))
+            data = cursor.fetchall()
+    
+
+    return render_template('data.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
