b49139e1ff2a342cc867ff9c392cce9edb4ddea5
leninware <churilov.ser1204@gmail.com>
2022-12-07 02:06:35 +0300

    db connection 1

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ connection = psycopg2.connect(
     password = os.getenv("PGPASSWORD"),                                  
     host = os.getenv("PGHOST"),                                            
     port = os.getenv("PGPORT"),                                          
-    database = os.getenv("PGDATABASE")   
+    database = os.getenv("PGDATABASE"),   
     url = os.getenv("DATABASE_URL")                                    
 )          
 # url = os.getenv("DATABASE_URL")
